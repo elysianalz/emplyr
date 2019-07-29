e510c018de010cef0d5f0fea4ab79304990b0761
matthew <mtk2011@live.com.au>
2019-07-29 20:36:43 +1000

    add login in as user button for previously logged in user (pre check for user)

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Facebook } from 'expo';
-import { StyleSheet, Text, View, TouchableOpacity, Alert, AsyncStorage} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, AsyncStorage, Image } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
 export default class LoginScreen extends React.Component {
@@ -12,14 +12,46 @@ export default class LoginScreen extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      userInfo: null,
+      user: this.get('id'),
     };
   }
 
+  // store a global value
   store(key, value){
     AsyncStorage.setItem(key, value);
   };
 
+  // storage helper - async storage wrapper
+  async _sh(key){
+    return await AsyncStorage.getItem(key);
+  }
+
+  // gets a stored item from async storage
+  get(key) {
+    let shItem = this._sh(key).then(item => {
+      this.getUserInformation(item);
+    })
+    .catch(error => console.error(error));
+  }
+
+  // retrieve user information based on .get key
+  getUserInformation(id){
+
+    fetch('http://192.168.0.16:3000/api/profile?id='+id)
+
+    .then((response) => response.json())
+
+    .then((responseJson) => {
+      this.setState({
+        user: responseJson,
+      });
+    })
+
+    .catch(error => console.error(error));
+
+  }
+
+  // add a new user to the db
   createUser(id, name, picture){
     fetch('http://192.168.0.16:3000/api/profile', {
       method: 'POST',
@@ -39,6 +71,12 @@ export default class LoginScreen extends React.Component {
     .catch(error => console.log('Error:', error));
   }
 
+  // procede as currently logged in user
+  procede(){
+    this.props.navigation.navigate('Home');
+  }
+
+  // facebook login
   async logIn() {
 
     try {
@@ -59,23 +97,15 @@ export default class LoginScreen extends React.Component {
         .then((response) => response.json())
 
         .then((responseJson) => {
-          /*this.store('user', responseJson.name);*/
           this.store('id', responseJson.id);
           this.createUser(responseJson.id, responseJson.name, JSON.stringify(responseJson.picture));
-          console.log("NAME: "+responseJson.name);
-          console.log("PICTURE: "+responseJson.picture);
-          console.log("object: ", JSON.stringify(responseJson));
           this.props.navigation.navigate('Home');
         })
 
         .catch((error) => {
           console.log(error);
         });
-
-      } else {
-        // type === 'cancel'
       }
-
     } catch ({ message }) {
       alert(`Facebook Login Error: ${message}`);
     }
@@ -83,15 +113,39 @@ export default class LoginScreen extends React.Component {
   }
 
   render() {
+
     const {navigate} = this.props.navigation;
+    let picture = "";
+    this.state.user ? picture = JSON.parse(this.state.user.picture) : null
     return (
+
       <View style={styles.homeScreen}>
+
         <Text style={styles.homeTitle}>Emplyr.</Text>
-        <TouchableOpacity style = {styles.facebookLoginBtn} onPress = {()=> this.logIn()}>
-          <AntDesign name="facebook-square" size={40} color="white" />
-          <Text style={styles.whiteText}>Login with Facebook</Text>
-        </TouchableOpacity>
+
+        {this.state.user ?
+
+          // procede with profile 
+          <TouchableOpacity style={styles.facebookLoginBtn} onPress={() => this.procede()}>
+            <Image 
+              style={{width: 50, height: 50}} 
+              source={{uri: picture.data.url}}
+            />
+            <Text style={styles.whiteText}>login as {this.state.user.name}</Text>
+          </TouchableOpacity>
+
+        :
+
+          // inital login with facebook
+          <TouchableOpacity style = {styles.facebookLoginBtn} onPress = {()=> this.logIn()}>
+            <AntDesign name="facebook-square" size={40} color="white" />
+            <Text style={styles.whiteText}>Login with Facebook</Text>
+          </TouchableOpacity>
+
+        }
+        
       </View>
+
     );
   }
 }
--- screens/work/workProfile.js
+++ screens/work/workProfile.js
@@ -16,10 +16,13 @@ export default class WorkProfile extends React.Component{
     }
   }
 
+
+  // async storage wrapper
   async _sh(key){
     return await AsyncStorage.getItem(key);
   }
 
+  // retrieve async storage item through _sh
   get(key) {
     let shItem = this._sh(key).then(item => {
       this.getUserInformation(item);
@@ -27,9 +30,9 @@ export default class WorkProfile extends React.Component{
     .catch(error => console.error(error));
   }
 
+  // set user information through .get 
   getUserInformation(id){
 
-  
     fetch('http://192.168.0.16:3000/api/profile?id='+id)
 
     .then((response) => response.json())
@@ -45,31 +48,47 @@ export default class WorkProfile extends React.Component{
   }
 
   render(){
+
+    //navigation prop
     const {navigate} = this.props.navigation;
+
+    //load picture and parse into json
     let picture = "";
     {this.state.user ? picture = JSON.parse(this.state.user.picture) : null}
     console.log(picture);
+
     return (
+
+      // profile picture
       <View style={styles.container}>
+
         {this.state.user ? 
           <Image 
             style={{width: '100%', height: '50%'}} 
             source={{uri: picture.data.url}}
           />
-        :null}
+        :
+          null
+        }
         
+        {/* edit profile button */}
         <View style={styles.button}>
           <Feather name="edit-2" 
                   size={45} 
                   color="white" 
                   onPress = {()=> this.props.navigation.navigate('EditProfile')} />
         </View>
+
+        {/* profile user name */}
         <Text style={styles.textContainer}>
          {this.state.user ? this.state.user.name : null}
         </Text>
+
+        {/* profile user description */}
         <Text style={styles.textContainer}>
           {this.state.user ? this.state.user.description : "edit your profile to add a description"}
         </Text>
+
       </View>
     );
   }
